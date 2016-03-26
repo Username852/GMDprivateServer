09e5e601e61f358c09e40003800f78468675ff64
Username852 <ijjo.baz@gmail.com>
2016-03-26 16:10:04 +0000

    Update getGJUserInfo20.php

--- getGJUserInfo20.php
+++ getGJUserInfo20.php
@@ -10,5 +10,5 @@ include "connection.php";
 	$creatorpoints = $user["creatorPoints"];
 	$youtubeurl = "";
 	//echo
-	echo "1:".$user["userName"].":2:".$user["userID"].":13:".$user["coins"].":17:".$user["userCoins"].":10:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":4:".$user["demons"].":8:".$creatorpoints.":18:0:19:0:20:".$youtubeurl.":21:".$user["accIcon"].":22:".$user["accShip"].":23:".$user["accBall"].":24:".$user["accBird"].":25:".$user["accDart"].":26:".$user["accRobot"].":28:".$user["accGlow"].":30:69:16:".$user["extID"].":31:0:38:0:39:0:40:0:29:1";
+	echo "1:".$user["Gamerul123"].":2:".$user["15141530"].":13:".$user["coins"].":0:".$user["userCoins"].":0:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":0:".$user["demons"].":0:".$creatorpoints.":18:0:19:0:20:".$youtubeurl.":21:".$user["accIcon"].":22:".$user["accShip"].":23:".$user["accBall"].":24:".$user["accBird"].":25:".$user["accDart"].":26:".$user["accRobot"].":28:".$user["accGlow"].":30:69:16:".$user["extID"].":31:0:38:0:39:0:40:0:29:1";
 ?>	
